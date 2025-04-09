f984149f9558f8909655d244e2f61dfea13f535f
FilippeHarnack <filippeharnack@gmail.com>
2025-04-09 10:11:50 -0300

    capabilities

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,7 @@ require 'rubygems'
 require 'selenium-webdriver'
 require 'cucumber'
 require 'faker' 
+
+caps = Yaml.load(File.read(File.join(File.dirname(__FILE__), 'capsAndroid.yml')))
+Appium::Driver.new(caps, true)
+Appium.promote_appium_methods Object
